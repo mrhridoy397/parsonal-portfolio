00550e04d01185e7298495f6139845937a862348
mrhridoy397 <mrhridoy397@gmail.com>
2023-11-14 13:47:28 -0700

    portfoliodetails model Controller Complete

---
+++ admin/controller/portfoliodetailsController.php
@@ -0,0 +1,160 @@
+<?php
+require_once(__dir__ . '/controller.php');
+require_once('./model/portfoliodetailsModel.php');
+require_once('./controller/UploadFile.php');
+class portfoliodetailsController extends Controller
+{
+
+    public $active = 'portfoliodetails'; //for highlighting the active link...
+    private $Model;
+
+    /**
+     * @param null|void
+     * @return null|void
+     * @desc Checks if the user session is set and creates a new instance of the DashboardModel...
+     **/
+    public function __construct()
+    {
+        if (!isset($_SESSION['auth_status'])) header("Location: index.php");
+        $this->Model = new portfoliodetailsModels();
+    }
+    
+    /**
+     * @param null|void
+     * @return array
+     * @desc Returns an array of news by calling the  BatchModel fetchNews method...
+     **/
+    public function getindexportfoliodetails(): array
+    {
+        return $this->Model->indexportfoliodetails();
+    }
+
+
+    // portfolio
+    public function getportfolio(){
+        return $this->Model->portfolio();
+    }
+    /**
+     * @param array
+     * @return array|boolean
+     * @desc Creates, and returns a user by calling the create method on the BatchModel...\
+     **/
+    public function createportfoliodetails($data, $file)
+    {
+       if($file['image'] != ""){
+         $fileName = new uploads();
+        $fileName->startupload($file);
+        $fileName->uploadfile();
+        $image  =  $fileName->dbupload;
+       }
+       else{
+        $image = "";
+       }
+
+      
+        $Payload = array(
+            'categorisId' => $data['categorisId'],
+            'title' => $data['title'],
+            'shortTitle' => $data['shortTitle'],
+            'Category' => $data['Category'],
+            'Client' => $data['Client'],
+            'Projectdate' => $data['Projectdate'],
+            'ProjectURL' => $data['ProjectURL'],
+            'description' => $data['description'],
+            'image' => $image,
+            'status' => $data['status'],
+
+            
+        );
+        $Response = $this->Model->portfoliodetailsCreate($Payload);
+
+        if (!$Response['status']) {
+            $Response['status'] = 'Sorry, An unexpected error occurred and your request could not be completed.';
+            return $Response;
+        }
+        else{
+            $Response['status'] = 'Congress! you data added successfully';
+            header("location: ./GalaryIndex.php");
+            return $Response;
+        }
+    }
+
+    /**
+     * @param null|void
+     * @return array
+     * @desc Returns an array of news by calling the  BatchModel fetchNews method...
+     **/
+    public function portfoliodetailsedit($id): array
+    {
+        return $this->Model->editportfoliodetails($id);
+    }
+
+    public function portfoliodetailsUpdate(array $data, $file)
+    {
+        if ($file['image']['name'] == "") {
+            $image = $data['oldImage'];
+        } 
+        else {
+            if ($data['oldImage'] != "") {
+                unlink($_SERVER['DOCUMENT_ROOT'] . "/iPortfolio-project/" . $data['oldImage']);
+            }
+            $fileName = new upload();
+            $fileName->startupload($file);
+            $fileName->uploadfile();
+            $image  =  $fileName->dbupload;
+        }
+
+       
+        $Payload = array(
+            'id' => $data['id'],
+            'categorisId' => $data['categorisId'],
+            'title' => $data['title'],
+            'shortTitle' => $data['shortTitle'],
+            'Category' => $data['Category'],
+            'Client' => $data['Client'],
+            'Projectdate' => $data['Projectdate'],
+            'ProjectURL' => $data['ProjectURL'],
+            'description' => $data['description'],
+            'image' => $image,
+            'status' => $data['status'],
+        );
+        $Response = $this->Model->Updateportfoliodetails($Payload);
+
+        if (!$Response['status']) {
+            $Response['status'] = 'Sorry, An unexpected error occurred and your request could not be completed.';
+            return $Response;
+        }
+        else{
+            $Response['status'] = 'Congress! you data Update successfully';
+            header("location: ./GalaryIndex.php");
+            return $Response;
+        }
+    }
+
+
+    /**
+     * @param null|void
+     * @return array
+     * @desc Returns an array of news by calling the  BatchModel fetchNews method...
+     **/
+    public function portfoliodetailsDelete($id)
+    {
+        $image = $this->Model->deleteImage($id);
+        if($image['image'] != false){
+            unlink($_SERVER['DOCUMENT_ROOT']."/iPortfolio-project/".$image['image']);
+        }
+
+        $response = $this->Model->deleteportfoliodetails($id);
+        if(!$response){
+            $Response['status'] = 'Sorry, An unexpected error occurred and your request could not be completed.';
+            header("location: ./GalaryIndex.php");
+            return $Response;
+        }
+        else{
+            $Response['status'] = 'Congress! you data Update successfully';
+            header("location: ./GalaryIndex.php");
+            return $Response;
+        }
+    }
+
+}
---
+++ admin/model/portfoliodetailsModel.php
@@ -0,0 +1,159 @@
+<?php
+require_once(__dir__ . '/Db.php');
+class portfoliodetailsModels extends Db
+{
+       /**
+     * @param string
+     * @return array
+     * @desc Returns a user record based on the method parameter....
+     **/
+    public function indexportfoliodetails()
+    {
+        $this->query("SELECT * FROM `table_galary`");
+        $this->execute();
+
+        $galary = $this->fetchAll();
+        if (!empty($galary)) {
+            $Response = array(
+               $galary
+            );
+            return $Response;
+        }
+        return array(
+          
+        );
+    }
+
+    // portFolio
+    public function portfolio()
+    {
+        $this->query("SELECT * FROM `portfolio` ");
+        $this->execute();
+        $portfolio = $this->fetchAll();
+        if (!empty($portfolio)) {
+            return $portfolio;
+        }
+        return array();
+    }
+    /**
+     * @param array
+     * @return array
+     * @desc Creates and returns a user record....
+     **/
+    public function portfoliodetailsCreate(array $data)
+    {
+        $this->query("INSERT INTO `portfoliodetails`(`categorisId`, `title`, `shortTitle`, `Category`, `Client`, `Projectdate`, `ProjectURL`, `description`, `image`, `status`) VALUES (?,?,?,?,?,?,?,?,?,?)");
+        $this->bind(1, $data['categorisId']);
+        $this->bind(2, $data['title']);
+        $this->bind(3, $data['shortTitle']);
+        $this->bind(4, $data['Category']);
+        $this->bind(5, $data['Client']);
+        $this->bind(6, $data['Projectdate']);
+        $this->bind(7, $data['ProjectURL']);
+        $this->bind(8, $data['description']);
+        $this->bind(9, $data['image']);
+        $this->bind(10, $data['status']);
+        
+
+        if ($this->execute()) {
+            $Response = array(
+                'status' => true,
+            );
+            return $Response;
+        } else {
+            $Response = array(
+                'status' => false
+            );
+            return $Response;
+        }
+    }
+
+ 
+
+
+      /**
+     * @param string
+     * @return array
+     * @desc Returns a user record based on the method parameter....
+     **/
+    public function editportfoliodetails($id)
+    {
+        $this->query("SELECT * FROM `portfoliodetails` WHERE`id` = :id");
+        $this->bind('id', $id);
+        $this->execute();
+
+        $portfoliodetails = $this->fetch();
+        if (!empty($portfoliodetails)) {
+              return  $portfoliodetails; 
+        }
+    }
+
+
+      /**
+     * @param array
+     * @return array
+     * @desc Creates and returns a user record....
+     **/
+    public function Updateportfoliodetails(array $data): array
+    {
+        $this->query("UPDATE `portfoliodetails` SET `categorisId`=?,`title`=?,`shortTitle`=?,`Category`=?,`Client`=?,`Projectdate`=?,`ProjectURL`=?,`description`=?,`image`=?,`status`=? WHERE `id`=?");
+        $this->bind(1, $data['categorisId']);
+        $this->bind(2, $data['title']);
+        $this->bind(3, $data['shortTitle']);
+        $this->bind(4, $data['Category']);
+        $this->bind(5, $data['Client']);
+        $this->bind(6, $data['Projectdate']);
+        $this->bind(7, $data['ProjectURL']);
+        $this->bind(8, $data['description']);
+        $this->bind(9, $data['image']);
+        $this->bind(10, $data['status']);
+        $this->bind(11, $data['id']);
+
+        if ($this->execute()) {
+            $Response = array(
+                'status' => true,
+            );
+            return $Response;
+        } else {
+            $Response = array(
+                'status' => false
+            );
+            return $Response;
+        }
+    }
+
+
+    public function deleteImage($id){
+        $this->query("SELECT `image`FROM `portfoliodetails` WHERE `id` = :id");
+        $this->bind('id', $id);
+        $this->execute();
+        $image = $this->fetch();
+        if ($image) {
+            return $image;
+        } else {
+            return false;
+        }
+    }
+        /**
+     * @param string
+     * @return array
+     * @desc Returns a user record based on the method parameter....
+     **/
+    public function deleteportfoliodetails($id): array
+    {
+        $this->query("DELETE FROM `portfoliodetails` WHERE `id` = :id");
+        $this->bind('id', $id);
+        if ($this->execute()) {
+            $Response = array(
+                'status' => true,
+                'msg' =>'portfoliodetails Delete successfully'
+            );
+            return $Response;
+        } else {
+            $Response = array(
+                'status' => false
+            );
+            return $Response;
+        }
+    }
+}
